e3ac69295d5c2359ea51e138d203d6b9df32f2ed
CTheCheese <colby.hunter42@gmail.com>
2023-06-23 04:08:42 -0500

    Added bones of settings

--- main.py
+++ main.py
@@ -7,12 +7,12 @@ from mpx_img_interface import add_border_to_all_images
 parent_directory = os.getcwd()
 
 def full_process():
-    # # Loads creatures from Assets file
-    # creatures = load_creatures()
-    # # Creates folders to extract files into
-    # creatures_dir = create_folders(creatures, "creatures")
-    # # TODO: Automate extracting files from Assets.xml, currently you must export manually
-    # remove_spaces_from_files(os.path.join(parent_directory, "creatures"))
+    # Loads creatures from Assets file
+    creatures = load_creatures()
+    # Creates folders to extract files into
+    creatures_dir = create_folders(creatures, "creatures")
+    # TODO: Automate extracting files from Assets.xml, currently you must export manually
+    remove_spaces_from_files(os.path.join(parent_directory, "creatures"))
 
     creatures_dir = os.path.join(parent_directory, "testing")
 
@@ -22,4 +22,15 @@ def full_process():
     for bi in bordered_images:
         move_file(bi, target_dir)
 
-full_process()
+settings = {
+    "asset_file": "",
+    "asset_name_idx": 0,
+    "asset_output_dir": "",
+    "extraction_name_idx": 0,
+    "magnification": 1,
+    "bordered_output_dir": "",
+    "borderless_output_dir": "",
+    "bordered_file_extention": "bordered",
+    "borderless_file_extension": "borderless",
+    "include_magnification_in_extension": True
+}
